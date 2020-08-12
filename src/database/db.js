b994ce96769d1215e5a1c9a51995e16feabeef57
jardelino Medeiros <jardelinomedeiros@outlook.com>
2020-08-12 10:19:39 -0300

    Finish

--- src/database/db.js
+++ src/database/db.js
@@ -13,7 +13,7 @@ function execute(db) {
         
         CREATE TABLE IF NOT EXISTS classes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            subject TEXT,
+            subject INTEGER,
             cost TEXT,
             proffys_id INTEGER
         );
--- src/database/test.js
+++ src/database/test.js
@@ -12,7 +12,7 @@ Database.then(async (db) => {
     }
 
     classValue = {
-        subject:"Química",
+        subject:1,
         cost:"20",
         // o proffy id virá pelo banco de dados
     }
@@ -31,7 +31,35 @@ Database.then(async (db) => {
         }
     ]
 
-     await createProffy(db, {proffyValue, classValue, classScheduleValues})
+    //  await createProffy(db, {proffyValue, classValue, classScheduleValues})
 
     //consultar os dados inseridos
+
+    //todos os proffys
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    // console.log(selectedProffys)
+
+    //consultar as classes de um determinado professor
+    // e trazer junto os dados do professor
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffys_id = proffys.id)
+        WHERE classes.proffys_id = 1;
+    `)
+    // console.log(selectClassesAndProffys)
+
+
+
+
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "1300"
+        AND class_schedule.time_to > "1300"
+    `)
+
+    // console.log(selectClassesSchedules)
 })
